bfc7d01ad0bb9670098d439118e7bcf50473cb02
David Lores <loresito@gmail.com>
2020-10-26 22:34:22 +0100

    Update SafeConfig configurations

--- src/SafeConfig.php
+++ src/SafeConfig.php
@@ -20,10 +20,15 @@ class SafeConfig extends Config
             '@PHP56Migration' => true,
             '@PHP70Migration' => true,
             '@PHP71Migration' => true,
+            '@PHP73Migration' => true,
             '@Symfony' => true,
+            '@PSR2' => true,
+            '@PhpCsFixer' => true,
 
             'align_multiline_comment' => true,
-            'array_syntax' => ['syntax' => 'short'],
+            'array_syntax' => [
+                'syntax' => 'short'
+            ],
             'blank_line_before_statement' => true,
             'combine_consecutive_issets' => true,
             'combine_consecutive_unsets' => true,
@@ -33,7 +38,19 @@ class SafeConfig extends Config
             'heredoc_to_nowdoc' => true,
             'list_syntax' => ['syntax' => 'long'],
             'method_argument_space' => ['ensure_fully_multiline' => true],
-            'no_extra_consecutive_blank_lines' => ['tokens' => ['break', 'continue', 'extra', 'return', 'throw', 'use', 'parenthesis_brace_block', 'square_brace_block', 'curly_brace_block']],
+            'no_extra_consecutive_blank_lines' => [
+                'tokens' => [
+                    'break',
+                    'continue',
+                    'extra',
+                    'return',
+                    'throw',
+                    'use',
+                    'parenthesis_brace_block',
+                    'square_brace_block',
+                    'curly_brace_block'
+                ]
+            ],
             'no_null_property_initialization' => true,
             'no_short_echo_tag' => true,
             'no_superfluous_elseif' => true,
@@ -51,6 +68,9 @@ class SafeConfig extends Config
             'yoda_style' => true,
             'method_chaining_indentation' => true,
             'phpdoc_line_span' => ['const' => 'single', 'property' => 'single'],
+            'concat_space' => [
+                'spacing' => 'one',
+            ],
         ];
     }
 }
